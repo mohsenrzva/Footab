4c5c581ed8be4a8dd0233280ba16b1ef8b7af8a3
M.Rezvani <mohsenrzva@gmail.com>
2025-06-27 02:31:52 +0330

    Add global error handling and response interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   const config = new DocumentBuilder()
     .setTitle('Footab API')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
--- src/modules/auth/jwt-auth.guard.ts
+++ src/modules/auth/jwt-auth.guard.ts
@@ -1,12 +1,5 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  handleRequest(err: any, user: any, info: any, ctx: ExecutionContext) {
-    if (err || !user) {
-      return null;
-    }
-    return user;
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
